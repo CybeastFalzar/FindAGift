3f93c3a52ba3451bb3a1e1066b4eda0d01a18f05
CybeastFalzar <videshncollege@gmail.com>
2023-05-05 19:06:37 -0400

    HTML Shell Updated

--- Project/app.py
+++ Project/app.py
@@ -1,23 +1,37 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 from filter import filter_data
 
 app = Flask(__name__)
 
-@app.route("/home")
-def hello_world():
-    return "<p>Hello, Home!</p>"
-    
-@app.route("/")
-def default():
-    return "<p>Hello, Default!</p>"
+result_item = []
+test_item = filter_data("pringles.json")
+test_item_2 = filter_data("pringles.json")
 
-class Item:
-    def __init__(self, name, img_link, price):
-        self.name = name
-        self.img_link = img_link
-        self.price = price
+@app.route("/result")
+def result():
+    return render_template("results.html", content=result_item)
 
-Item1 = Item("Pringles", "https://pics.walgreens.com/prodimg/595138/450.jpg", "$10.50")
+@app.route("/find", methods=["POST", "GET"])
+def find():
+    if request.method == "POST":
+        chosen_interest = request.form["interest"]
+        if (chosen_interest=="pringles"):
+            result_item.append(test_item)
+            result_item.append(test_item_2)
+        return redirect(url_for("result"))
+    else:
+        result_item.clear()
+        return render_template("index.html") 
+
+    #return render_template("index.html", content = test_item)
+
+# class Item:
+#     def __init__(self, name, img_link, price):
+#         self.name = name
+#         self.img_link = img_link
+#         self.price = price
+
+# Item1 = Item("Pringles", "https://pics.walgreens.com/prodimg/595138/450.jpg", "$10.50")
 
 
 if __name__ == "__main__":
--- Project/filter.py
+++ Project/filter.py
@@ -32,6 +32,6 @@ def filter_data(file_name):
     result["price"] = median_offer_dict.get("price")
     result["buy_link"] = median_offer_dict.get("link")
     result["img_link"] = data['items'][0]['images'][num_dict.get(offer_median)]
-    print(result)
+    return result
     
 filter_data("pringles.json")
